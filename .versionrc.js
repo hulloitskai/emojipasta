c87e5aa76c669c8929990e192f234a1212b79e5b
Steven Xie <dev@stevenxie.me>
2020-10-24 20:35:30 -0400

    Configure releases with standard-version

---
+++ .versionrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "package.json",
+    },
+    {
+      filename: "app.config.json",
+      updater: require.resolve("standard-version-expo"),
+    },
+    {
+      filename: "app.config.json",
+      updater: require.resolve("standard-version-expo/ios/increment"),
+    },
+    {
+      filename: "app.config.json",
+      updater: require.resolve("standard-version-expo/android/increment"),
+    },
+  ],
+};
